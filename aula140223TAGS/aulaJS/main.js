9d4412090a76f3a7be7d6fb4dc0269bc5e41c585
magalhes <magalhes5@gmail.com>
2023-02-15 14:20:17 -0300

    Aula 150203

---
+++ aula140223TAGS/aulaJS/main.js
@@ -0,0 +1,91 @@
+//Dia 14.02
+//Variaveis e operadores
+
+/*let nomeCompeto;
+nomeCompeto = "Lucas Leao"
+console.log(nomeCompeto)
+console.log("CHips")
+nomeCompeto = "Leão Lucas"
+console.log(nomeCompeto)
+let idade = 19
+idade = 26
+console.log(idade)
+console.log("Meu nome é " + nomeCompeto + " e tenho " + idade)
+
+let golsSofrido = 3; //numero
+let chaceDerrota = 0.55//numero decimal
+let posseBola = 33.57
+let nomeTime = "Valencia"
+let patrocinio
+patrocinio = "SoulCode"
+console.log(patrocinio)
+
+//boolean = 2 valores possiveis(true ou false)
+let timeGanhou = true
+let timePerdeu = false
+
+//Operadores aritmeticos
+let soma = 200+100
+let sub = 100-55
+let mult = 5*7
+let div = 400/25
+let resto = 8%3
+let expressao = (soma+sub) * div
+let expressao2 = (mult - div) / resto
+console.log(soma)
+console.log(`A subtração e ${sub}`) // template string
+console.log(`A multiplicação é ${mult}`)
+console.log(`A divisão é ${div}`)
+console.log(resto)
+console.log(expressao)
+console.log(expressao2)*/
+let i = 0
+//i = i + 1 // o novo valor i é 0 + 1 = 1
+//i = i + 1 // o novo valor i é 1 + 1 = 2
+//i = i + 1 // o novo valor i é 0 + 1 = 1
+//i = i + 1 // o novo valor i é 3 + 1 = 4
+// i++
+// console.log(i)
+// i++ // altera o valor de i somando i+1 = incremento
+// console.log(i)
+// i-- //i = i * 1 = decremento
+// console.log(i)
+
+//operadores relacionais
+// let a = 3
+// let b = 5
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a == b)
+// console.log(a != b)
+
+// console.log( a == "3")
+// console.log( a === "3")
+//  O operador === compara o valor ao tipo
+
+
+//Exercicio 1
+let peso = 88.9
+let altura = 1.76
+let imc = peso /(altura * altura)
+console.log(`Meu IMC é ${imc.toFixed()}`)
+
+//Exercicio 2
+const pi = 3.14
+let raio = 7
+let circulo = pi * raio *raio
+console.log(`O raio é ${circulo.toFixed()}`)
+
+//Exercicio 3
+let circuferencia = 2 * pi * raio
+console.log(`A circuferencia é ${circuferencia.toFixed()}`)
+
+//Exercicio 4
+let celsius = 32.4
+let graus = celsius * 1.8 + 32
+console.log(`A temperatura é ${graus.toFixed()}`)
+
+
+
